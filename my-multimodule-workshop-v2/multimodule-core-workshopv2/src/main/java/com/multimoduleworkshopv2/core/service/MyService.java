fa3090b8f6c43071d39e1673763a03a60992cd28
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2020-02-14 18:29:58 +0600

    update v2 as medicare

--- my-multimodule-workshop-v2/multimodule-core-workshopv2/src/main/java/com/multimoduleworkshopv2/core/service/MyService.java
+++ my-multimodule-workshop-v2/multimodule-core-workshopv2/src/main/java/com/multimoduleworkshopv2/core/service/MyService.java
@@ -7,7 +7,7 @@ public class MyService {
 	
 	public String getMsg1(){
 		
-		System.out.println("This message from Workshop v2 core MyService Ms1  up00   =====---##@@2");
+		System.out.println("This message from Workshop v2 core MyService Ms1  === Ok 2");
 		
 		return "This message from Workshop v1 core MyService Ms1";
 		
@@ -15,7 +15,7 @@ public class MyService {
 	
 	public String getMsg2(){
 		
-		System.out.println("This message from Workshop v2 core MyService Ms2  up00 ======---##@@2");
+		System.out.println("This message from Workshop v2 core MyService Ms2 === ok 2");
 		
 		return "This message from Workshop v1 core MyService Ms2";
 		
